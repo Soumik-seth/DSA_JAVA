5114714d6293efaa64f4a3f0e2171fbdba3e177f
Soumik seth <soumikseth4@gmail.com>
2026-01-06 22:49:16 +0530

    I am working

---
+++ LINKED_LIST/single_Link_list_display.java
@@ -0,0 +1,28 @@
+package LINKED_LIST;
+
+public class single_Link_list_display {
+public static class Node {
+    int data;
+    Node next;
+    public Node(int data){
+        this.data = data;
+  
+    }
+
+
+}
+Node head ;
+void insert(int data){
+    Node newNode = new Node(data);
+    if(head ==  null){
+        head = newNode;
+        return;
+    }
+    Node temp = head;
+    while()
+}
+
+    public static void main(String[] args) {
+        
+    }
+}
