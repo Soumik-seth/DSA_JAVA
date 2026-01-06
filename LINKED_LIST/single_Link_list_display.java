a62586ed8aa90b50301c79f9ab66adb9e6cbbfbb
Soumik seth <soumikseth4@gmail.com>
2026-01-06 23:12:50 +0530

    I have done single linked list

--- LINKED_LIST/single_Link_list_display.java
+++ LINKED_LIST/single_Link_list_display.java
@@ -19,10 +19,28 @@ void insert(int data){
         return;
     }
     Node temp = head;
-    while()
-}
-
-    public static void main(String[] args) {
-        
+    while(temp.next != null){
+        temp = temp.next;
     }
+    temp.next = newNode;
+}
+void display(){
+    Node temp = head;
+    while(temp != null){
+        System.out.print(temp.data + " -> ");
+        temp = temp.next;
+    }   
+    System.out.println("null");
+}
+    public static void main(String[] args) {
+        single_Link_list_display list = new single_Link_list_display();
+
+        list.insert(10);
+        list.insert(20);
+        list.insert(30);
+        list.insert(40);
+
+        list.display();
+    }
+
 }
