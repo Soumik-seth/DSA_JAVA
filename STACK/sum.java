40b9669b83afeaff1d3dda6752c73ebef6dd2702
Soumik seth <soumikseth4@gmail.com>
2026-01-28 20:26:17 +0530

    DSA

--- STACK/sum.java
+++ STACK/sum.java
@@ -1,30 +1,46 @@
 package STACK;
 import java.util.*;
 
+public class sum{
 
-public class sum {
-    public static int countsum(int max,int[] a,int[] b){
-        return helper(max,a,b,0,0)-1;
+    public static int countsum(int max, int[] a, int[] b) {
+        return helper(max, a, b, 0, 0);
     }
 
-    public static int helper(int max,int []a,int []b,int count,int sum){
-        if(sum>max){
-            return count;
+    public static int helper(int max, int[] a, int[] b, int count, int sum) {
+        // Stop if sum exceeds max
+        if (sum > max) {
+            return count - 1;
         }
 
- if(a.length == 0 && b.length == 0){
-            return count;
+        int ans = count;
+
+        // Take from a if possible
+        if (a.length > 0) {
+            ans = Math.max(ans,
+                    helper(max,
+                            Arrays.copyOfRange(a, 1, a.length),
+                            b,
+                            count + 1,
+                            sum + a[0]));
         }
 
-        int ans1=helper(max,Array.copyOfRange(a,1,a.length),b,count+1,sum+a[0]);
-        int ans2=helper(max,a,Array.copyOfRange(b,1,b.length),count+1,sum+b[0]);
-        return Math.max(ans1,ans2);
+        // Take from b if possible
+        if (b.length > 0) {
+            ans = Math.max(ans,
+                    helper(max,
+                            a,
+                            Arrays.copyOfRange(b, 1, b.length),
+                            count + 1,
+                            sum + b[0]));
+        }
+
+        return ans;
     }
-
 
     public static void main(String[] args) {
-        int [] a = {1,2,3};
-        int [] b = {4,5,6};
-        System.out.println(countsum(10,a,b));
+        int[] a = {1, 2, 3};
+        int[] b = {4, 5, 6};
+        System.out.println(countsum(10, a, b)); // Output: 4
     }
 }
